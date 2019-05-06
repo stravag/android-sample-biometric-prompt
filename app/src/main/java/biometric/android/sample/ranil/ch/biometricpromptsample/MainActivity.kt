c9830d082ff2c8349449582099d8068fba68188d
Ranil Wijeyratne </Users/ranil/Scripts/gitConfig.sh>
2019-05-06 20:23:39 +0200

    initial commit

---
+++ app/src/main/java/biometric/android/sample/ranil/ch/biometricpromptsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package biometric.android.sample.ranil.ch.biometricpromptsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
